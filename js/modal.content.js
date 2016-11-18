07c4608ac51c5827909e2083eacd0cc19ae84106
SunbeamEllen <sunshiningbrightly@mail.ru>
2016-11-18 16:50:29 +0200

    Коррекция кода

--- js/modal.content.js
+++ js/modal.content.js
@@ -4,7 +4,7 @@
 			var close = popup.querySelector(".content-close");
 			var form = popup.querySelector("form");
 			var login = popup.querySelector("[name=login]");
-			var password = popup.querySelector("[name=email]");
+			var password = popup.querySelector("[name=mail]");
 			var storage = localStorage.getItem("login");
 
 			link.addEventListener("click", function(event) {
@@ -14,32 +14,32 @@
 
 				if (storage) {
           		login.value = storage;
-          		email.focus();
+          		mail.focus();
         		} else {
          		login.focus();
         		}				
       		});
 
-      		close.addEventListener("click", function(event) {
+      	close.addEventListener("click", function(event) {
         	event.preventDefault();
         	popup.classList.remove("modal-content-show");
         	popup.classList.remove("modal-error");
         	overlay.classList.remove("modal-overlay-show");
       		});
 
-      		form.addEventListener("submit", function(event) {
-        	if (!login.value || !email.value) {
+      	form.addEventListener("submit", function(event) {
+        	if (!login.value || !mail.value) {
           	event.preventDefault();
           	popup.classList.remove("modal-error");
           	popup.offsetWidth = popup.offsetWidth;
           	popup.classList.add("modal-error");
          	console.log("Нужно ввести имя и почту"); 
         	}	else {
-          	localStorage.setItem("name", name.value);
+          	localStorage.setItem("login", login.value);
         	}
       		});
 
-      		window.addEventListener("keydown", function(event) {
+      	window.addEventListener("keydown", function(event) {
         	if (event.keyCode === 27) {
           	if (popup.classList.contains("modal-content-show")) {
             popup.classList.remove("modal-content-show");
